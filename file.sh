3795f1cb939940997b173f9697dd325f92b6d02c
Riyas MJ <riyas@giskernel.com>
2022-01-07 19:51:37 +0530

    Files

---
+++ file.sh
@@ -0,0 +1,6 @@
+rm /var/www/html/frizon/storage/app/public/swi/*.shp
+rm /var/www/html/frizon/storage/app/public/swi/*.dbf
+rm /var/www/html/frizon/storage/app/public/swi/*.shx
+echo other files deleted
+
+exit 0
