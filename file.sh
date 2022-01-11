ccb2f9524ca5f649ac860026a8bcbc271613b728
Riyas MJ <riyas@giskernel.com>
2022-01-11 18:55:18 +0530

    cleaning

--- file.sh
+++
@@ -1,6 +0,0 @@
-rm /var/www/html/frizon/storage/app/public/swi/*.shp
-rm /var/www/html/frizon/storage/app/public/swi/*.dbf
-rm /var/www/html/frizon/storage/app/public/swi/*.shx
-echo other files deleted
-
-exit 0
